6cf6d6be37d54da23fac1ffffdc5d75f464cf2de
RingSho <s.0v0xx@outlook.jp>
2020-03-29 22:15:10 +0900

    Add: view to create and edit

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,6 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(BASE_DIR)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
